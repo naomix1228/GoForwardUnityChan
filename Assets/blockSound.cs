1c78c82a0b3ebf5e76732909fb9aded9c5accfd8
NAOMI IMAOKA <naomi_t50@yahoo.co.jp>
2016-11-07 17:52:05 +0900

    よろしくお願いします

---
+++ Assets/blockSound.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class blockSound : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "UnityChan2D")
+        {
+            
+            GetComponent<AudioSource>().volume = 0;
+        }
+
+
+        AudioClip clip = gameObject.GetComponent<AudioSource>().clip;
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+        
+
+
+        
+        
+
+        
+
+
+
+
+
+    }
+}
